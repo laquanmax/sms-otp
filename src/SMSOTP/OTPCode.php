849b73ca8da13134728ebd01a69e5f456c73f004
laquanmax <agmaxwellh@gmail.com>
2018-08-25 18:52:56 -0400

    change from 5 to 6 character code

--- src/SMSOTP/OTPCode.php
+++ src/SMSOTP/OTPCode.php
@@ -6,7 +6,7 @@ use SMSOTP\Contract\Generator;
 
 class OTPCode implements Generator
 {
-    public function generate($length = 5) : string
+    public function generate($length = 6) : string
     {
         return strtoupper(substr(hash('sha256', strtotime('now')), 0, $length));
     }
