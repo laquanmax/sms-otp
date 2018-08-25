253f69614d7671f939c5e60b64d1ea0689f67c3b
laquanmax <agmaxwellh@gmail.com>
2018-08-25 19:42:35 -0400

    Update Repository.php

--- src/SMSOTP/Contract/Repository.php
+++ src/SMSOTP/Contract/Repository.php
@@ -4,7 +4,7 @@ namespace SMSOTP\Contract;
 
 interface Repository
 {
-    public function save_otp($number, $code);
+    public function save_otp($number, $code, $token);
     public function otp_details($number, $code);
     public function mark_otp_verified($code);
 }
